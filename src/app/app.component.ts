6a9b8dbb95f2bd8e5f5b4d434bb8b6d21b79a0c8
Dariusz <ddarrekk100@o2.pl>
2024-01-30 23:09:35 +0100

    created project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,6 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'angular_exam';
-}
+export class AppComponent {}
