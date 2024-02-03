83291d260ca8359c3a857c58a27aa3959309a2e2
Dariusz <ddarrekk100@o2.pl>
2024-02-03 04:05:57 +0100

    score list//problem with user.name

--- src/app/snake-containing-component/snake-containing-component.component.ts
+++ src/app/snake-containing-component/snake-containing-component.component.ts
@@ -3,23 +3,26 @@ import { NgxSnakeComponent, NgxSnakeModule } from 'ngx-snake';
 import { HotkeysService } from '@ngneat/hotkeys';
 import { CommonModule } from '@angular/common';
 import { User } from '../user';
+import { NgClass } from '@angular/common';
+import { FormComponent } from '../form/form.component';
 @Component({
   selector: 'app-snake-containing-component',
   standalone: true,
-  imports: [CommonModule, NgxSnakeModule],
+  imports: [CommonModule, NgxSnakeModule, NgClass, FormComponent],
   providers: [HotkeysService],
+
   templateUrl: './snake-containing-component.component.html',
   styleUrl: './snake-containing-component.component.scss',
 })
 export class SnakeContainingComponentComponent {
-  @Input() user: User | null = null;
-  public points: number = 0;
-
-  public bw = false;
-
   @ViewChild('game')
   private _snake!: NgxSnakeComponent;
-
+@Input() user: User | null = null;
+  public points: number = 0;
+  public scores: { score: number; time: number }[] = [];
+  public gameStartTime: number | null = null;
+  public gameDuration: number = 0;
+  public bw = false;
   constructor(private hotkeys: HotkeysService) {
     this._addHotkeys();
   }
@@ -38,12 +41,45 @@ export class SnakeContainingComponentComponent {
       .addShortcut({ keys: 'right' })
       .subscribe(() => this._snake.actionRight());
   }
+  public showUser() {
+    console.log(this.user);
+  }
+  public startGame() {
+    console.log(this.user);
+    console.log(`pokaz this.user.name ${this.user?.name}`);
+    console.log(`pokaz this.user.email ${this.user?.email}`);
+
+    this._snake.actionStart();
+    this.gameStartTime = performance.now();
+    this.gameDuration = 0;
+    this.updateGameDuration();
+  }
+  private updateGameDuration() {
+    if (this.gameStartTime !== null) {
+      const currentTime = performance.now();
+      this.gameDuration = Math.floor((currentTime - this.gameStartTime) / 1000);
+      requestAnimationFrame(() => this.updateGameDuration());
+    }
+  }
   public onGrow() {
     this.points += 1;
     console.log('grow');
   }
 
   public onGameOver() {
-    alert('game over');
+    if (this.gameStartTime !== null) {
+      const currentTime = performance.now();
+      this.gameDuration = Math.floor((currentTime - this.gameStartTime) / 1000);
+    }
+
+    const gameResult = {
+      score: this.points,
+      time: this.gameDuration,
+    };
+
+    this.scores.push(gameResult);
+    console.log('result to');
+    console.log(gameResult);
+    this.points = 0;
   }
 }
